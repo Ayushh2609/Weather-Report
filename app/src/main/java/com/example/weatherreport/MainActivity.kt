e6335522b8a55d7cbce8107b058f585e4619df33
Ayushh0708 <paliwalaayush717@gmail.com>
2025-06-05 05:35:47 +0530

    UI of weather app

--- app/src/main/java/com/example/weatherreport/MainActivity.kt
+++ app/src/main/java/com/example/weatherreport/MainActivity.kt
@@ -5,6 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,27 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        fetchWeatherData()
+    }
+
+    private fun fetchWeatherData() {
+        val retroFit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .build().create(ApiInterface::class.java)
+
+        val response = retroFit.getWeatherData("haridwar" , "35c7535df7dc23e1ac7b734f327610d2" ,"metric")
+
+        response.enqueue(object : Callback<WeatherApp> {
+            override fun onResponse(call: Call<WeatherApp>, response: Response<WeatherApp>) {
+
+            }
+
+            override fun onFailure(call: Call<WeatherApp>, t: Throwable) {
+
+            }
+
+        })
     }
 }
