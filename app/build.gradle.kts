1a45e00a19385e695b990034c5e2a08a7c453087
Ayushh0708 <paliwalaayush717@gmail.com>
2025-06-06 17:14:37 +0530

    UI of weather app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.weatherreport"
     compileSdk = 35
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.weatherreport"
         minSdk = 24
